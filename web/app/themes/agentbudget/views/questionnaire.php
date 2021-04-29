b27b0ac0926446f51592ba4a8a08c9a0378c1707
denis <denis@kaizen-design.ru>
2021-04-29 03:43:01 +0300

    Financial goals slice completed

--- web/app/themes/agentbudget/views/questionnaire.php
+++ web/app/themes/agentbudget/views/questionnaire.php
@@ -3,6 +3,10 @@
  * Template Name: Questionnaire
  */
 
+if ( !is_user_logged_in() ) {
+  auth_redirect();
+}
+
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
